bcdd50a14407747d7a175d90a7beb2e07104bd0b
Olga Belitskaya <safuolga@gmail.com>
2020-10-05 21:11:06 +0300

    add python_recipes/display_dataframe.py

---
+++ python_recipes/display_dataframe.py
@@ -0,0 +1,27 @@
+import numpy as np,pandas as pd
+from IPython.display import display
+
+def display_dataframe(df,cmap,dhead=False):
+    style_dict={'font-size':'130%',
+                'font-family':'times',
+                'text-shadow':'slategray 2px 2px 2px'}
+    head_styler=(('color','darkslategray'),
+	             ('font-family','times'),
+                 ('font-size','300%'),
+                 ('text-shadow','slategray 2px 2px 2px'))
+    if dhead=='head': data=df.head()
+    elif dhead=='tail': data=df.tail()
+    else: data=df
+    display(data.style.set_properties(**style_dict)\
+            .background_gradient(cmap=cmap)\
+            .set_table_styles([dict(selector='th',
+                                    props=head_styler)]));
+
+def display_dataframe_example():
+    index=[chr(i) for i in range(ord('a'),ord('h')+1)]
+    columns=['&#x2654;','&#x2655;','&#x2656;',
+             '&#x2657;','&#x2658;','&#x2659;']
+    df=pd.DataFrame(np.random.randn(8,6),
+                    index=index,columns=columns)
+    cmap='bone'
+    display_dataframe(df,cmap)
