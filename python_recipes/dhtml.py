d44994405f7b9b3b631815c49a3eab713b388218
Olga Belitskaya <safuolga@gmail.com>
2020-10-08 23:16:58 +0300

    update python_recipes/dhtml.py

--- python_recipes/dhtml.py
+++ python_recipes/dhtml.py
@@ -2,7 +2,7 @@ from IPython.display import display,HTML
 # Crayola Colors
 c1,c2,c3,c4,c5,c6,c7,c8,c9,c10=\
 '#FF355E','#FF6037','#FF9966','#FFCC33','#FFFF66',\
-'CCFF00','#66FF66','#50BFE6','#FF6EFF','#FF00CC'
+'#CCFF00','#66FF66','#50BFE6','#FF6EFF','#FF00CC'
 fs1,fs2,fs3,fs4,fs5,fs6,fs7,fs8=10,12,14,16,18,20,25,30
 f1,f2,f3,f4,f5,f6,f7=\
 'Smokum','Akronim','Wallpoet','Orbitron',\
