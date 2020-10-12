50302d7b3086749b554a324c0adaabd61c856241
Olga Belitskaya <safuolga@gmail.com>
2020-10-12 19:42:12 +0300

    add python_recipes/rate_map.py

--- python_recipes/rate_map.py
+++ python_recipes/rate_map.py
@@ -1,19 +1,22 @@
 import pandas as pd,pylab as pl
-fp='https://data.cityofnewyork.us/resource/'
-se=pd.read_json(fp+'h7rb-945c.json')
-fl=['graduation_rate','attendance_rate',
-    'latitude','longitude']
-se=se[fl].dropna()
-f,ax=pl.subplots(ncols=2,figsize=(11,4))
 def rate_map(cmap):
+    fp='https://data.cityofnewyork.us/resource/'
+    se=pd.read_json(fp+'h7rb-945c.json')
+    fl=['graduation_rate','attendance_rate',
+       'latitude','longitude']
+    se=se[fl].dropna()
+    f,ax=pl.subplots(ncols=2,figsize=(10,5),
+                     sharey=True)
+    f=pl.gcf()
     for i in range(2):
         se[fl[i]]=se[fl[i]].astype('string')\
         .str.replace('N/A','0').astype('float')
-        se.plot(kind='scatter',
+        se.plot(kind='scatter',ax=ax[i],
             x='longitude',y='latitude',
-            s=10,c=fl[i],
-            cmap=pl.get_cmap(cmap),
+            s=10,c=fl[i],alpha=.8,grid=True,
             title=fl[i].replace('_',' '),
-        ax=ax[i],alpha=.8,grid=True)
+            cmap=pl.get_cmap(cmap))
         ax[i].set_facecolor('slategray')
+        cax=f.get_axes()[i+2]
+        cax.set_ylabel('')
     pl.show()
