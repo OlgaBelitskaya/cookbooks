ff54f7f3bc171b9be77107cec56ff0e4b9f4f58c
Olga Belitskaya <safuolga@gmail.com>
2020-10-17 23:21:22 +0300

    add python_recipes/amplitude_wavfile.py

--- python_recipes/amplitude_wavfile.py
+++ python_recipes/amplitude_wavfile.py
@@ -1,4 +1,4 @@
-import numpy,pylab,urllib
+import numpy as np,pylab as plot,urllib
 from scipy.io import wavfile
 url='https://ccrma.stanford.edu/~jos/'+\
     'wav/slideflute.wav'
@@ -7,9 +7,9 @@ output_file=open('slideflute.wav','wb')
 output_file.write(input_file.read())
 output_file.close(); input_file.close()
 sr,wav=wavfile.read('slideflute.wav');
-wav=wav.astype('float')/numpy.iinfo(numpy.int16).max
-f,ax=pylab.subplots(1,figsize=(7,5))
+wav=wav.astype('float')/np.iinfo(np.int16).max
+f,ax=pl.subplots(1,figsize=(7,5))
 ax.plot(wav[:,1],c='#3636ff',lw=.05)
 ax.plot(wav[:,0],c='#ff36ff',lw=.05,alpha=.7)
 ax.set_ylabel('amplitude')
-pylab.grid(); pylab.show()
+pl.grid(); pl.show()
