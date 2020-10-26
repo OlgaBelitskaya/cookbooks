73bef7f369b858024d4fd6d700c24071612263e5
Olga Belitskaya <safuolga@gmail.com>
2020-10-26 11:19:48 +0300

    update python_recipes/randomgraph_shortestpath.py

--- python_recipes/randomgraph_shortestpath.py
+++ python_recipes/randomgraph_shortestpath.py
@@ -14,3 +14,23 @@ def random_graph(num_min=7,num_max=15):
                     for ce in curr_edges]
         edges+=[curr_edges]
     return dict(zip(nodes,edges))
+
+def random_shortestpath(graph):
+    n=len(graph.keys()); rg=nx.Graph()
+    for key in graph.keys():
+        for value in graph[key]:
+           rg.add_edge(key,value[0],
+                       length=value[1])
+    [start,end]=np.random.choice(
+        n,2,replace=False)
+    start=list(graph.keys())[start]
+    end=list(graph.keys())[end]
+    shortest_path=nx.shortest_path(
+        rg,start,end,
+        weight='length')
+    shortest_path_list=[]
+    for i in range(len(shortest_path)-1):   
+        shortest_path_list.append(
+            (shortest_path[i],shortest_path[i+1]))
+    print('start: '+start+'; end: '+end)
+    return rg,shortest_path,shortest_path_list
