d86671963af1736b9895631683890309a5863fda
Olga Belitskaya <safuolga@gmail.com>
2020-11-07 23:21:09 +0300

    add python_recipes/pictogram_photo_classify.py

---
+++ python_recipes/pictogram_photo_classify.py
@@ -0,0 +1,115 @@
+import os,pandas as pd,numpy as np
+import seaborn as sn,pylab as pl
+from IPython.display import display
+from tensorflow.keras.preprocessing \
+import image as tkimg
+from tensorflow.keras.datasets import cifar10
+img_size1,img_size2=32,96
+cmap1,cmap2='spring','autumn'
+fw='weights.best.hdf5'
+
+def images2array(files_path,img_size,
+                 preprocess=False,grayscale=False):
+    files_list=sorted(os.listdir(files_path))
+    n,img_array=len(files_list),[]
+    for i in range(n):
+        if i%round(.1*n)==0:
+            print('=>',end='',flush=True)
+        img_path=files_path+files_list[i]
+        if preprocess:
+            img=tkimg.load_img(
+                img_path,grayscale=grayscale)
+            img=tkimg.img_to_array(img)
+            img=tkimg.smart_resize(
+                img,(img_size,img_size))
+        else:
+            img=tkimg.load_img(
+                img_path,target_size=(img_size,img_size))
+            img=tkimg.img_to_array(img)
+        img=np.expand_dims(img,axis=0)/255
+        img_array.append(img)
+    return np.array(np.vstack(img_array),
+                    dtype='float32')
+
+def labels2array(files_path):
+    files_list=sorted(os.listdir(files_path))
+    files_split=np.array([el.split('_') 
+                          for el in files_list])
+    num_labels=files_split.shape[1]-1
+    labels=[files_split[:,i] 
+            for i in range(num_labels)]
+    labels=np.array(labels).astype('int32')
+    for i in range(num_labels):
+        label_set=list(set(labels[i]))
+        replace_dict=\
+        dict(zip(label_set,
+                 list(range(len(label_set)))))
+        labels[i]=[replace_dict.get(x,x) 
+                   for x in labels[i]]
+    return labels
+
+def get_data(files_path,img_size,
+             preprocess=False,grayscale=False):
+    images=images2array(files_path,img_size,
+                        preprocess,grayscale)
+    labels=labels2array(files_path)
+    return images,labels
+
+def get_cifar():
+    (images,labels),(_,_)=cifar10.load_data()
+    images=np.array(images,dtype='float32')/255
+    labels=labels.reshape(1,-1)
+    return images,labels
+
+def data2nnarrays(images,labels,names,cmap):
+    N=images.shape[0]; n=int(.1*N)
+    shuffle_ids=np.arange(N)
+    np.random.RandomState(12).shuffle(shuffle_ids)
+    images=images[shuffle_ids]
+    labels=np.array([labels[i][shuffle_ids]
+                     for i in range(labels.shape[0])])
+    x_test,x_valid,x_train=\
+    images[:n],images[n:2*n],images[2*n:]
+    y_test,y_valid,y_train=\
+    labels[:,:n],labels[:,n:2*n],labels[:,2*n:]
+    print('data outputs: ')
+    df=pd.DataFrame([[x_train.shape,x_valid.shape,x_test.shape],
+                     [x_train.dtype,x_valid.dtype,x_test.dtype],
+                     [y_train.shape,y_valid.shape,y_test.shape],
+                     [y_train.dtype,y_valid.dtype,y_test.dtype]],
+                    columns=['train','valid','test'],
+                    index=['image shape','image type',
+                           'label shape','label type'])
+    display(df)
+    print('distribution of labels: ')
+    idx=['labels %d'%(i+1) 
+         for i in range(labels.shape[0])]
+    df=pd.DataFrame(labels,index=idx).T
+    for i in range(labels.shape[0]):
+        df['name %d'%(i+1)]=\
+        [names[i][l] for l in labels[i]]
+    fig=pl.figure(figsize=(10,10))    
+    for i in range(labels.shape[0]):
+        ax=fig.add_subplot(labels.shape[0],1,i+1)
+        sn.countplot(x='name %s'%(i+1),data=df,
+                     palette=cmap,alpha=.5,ax=ax)
+    pl.show()       
+    return [x_train,x_valid,x_test,
+            y_train,y_valid,y_test,
+            y_train,y_valid,y_test]
+
+def display_images(images,labels,names,n):
+    fig=pl.figure(figsize=(10,n))
+    randch=np.random.choice(
+        images.shape[0],size=n,replace=False)
+    for i,idx in enumerate(randch):
+        ax=fig.add_subplot(
+            n//3,3,i+1,xticks=[],yticks=[])
+        ax.imshow(images[idx])
+        label=[labels[:,idx]]
+        name=[names[i][labels[i][idx]]
+              for i in range(labels.shape[0])]
+        ax.set_title('{} \n {}'\
+                     .format(str(label),str(name)),
+                     fontsize=10)
+    pl.show()
