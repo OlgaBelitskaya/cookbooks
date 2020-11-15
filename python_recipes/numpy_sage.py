9fc8505781716835114e29c7a4194a2c60fc0cca
Olga Belitskaya <safuolga@gmail.com>
2020-11-15 15:51:49 +0300

    update python_recipes/numpy_sage.py

--- python_recipes/numpy_sage.py
+++ python_recipes/numpy_sage.py
@@ -46,10 +46,14 @@ def sage_list_plot(array,labels=None,
            """ width="""+str(width+20)+"""></iframe></div>"""
     display(HTML(string))
 
-    def sage_list_plot_min(array,width,height,kw,kh):
+def sage_list_plot_min(array,labels=None,
+                       width=700,height=400,
+                       kw=.0095,kh=.0065):
     str_array=np.array2string(
         array,precision=8,separator=',',
         suppress_small=True)
+    if labels==None: 
+        labels=[i+1 for i in range(len(array))]
     html_str="""<html><head><meta charset='utf-8'>"""+\
     """<script src='https://sagecell.sagemath.org/"""+\
     """static/embedded_sagecell.js'>"""+\
@@ -61,11 +65,13 @@ def sage_list_plot(array,labels=None,
     """<div class='plot_min'><script type='text/x-sage'>"""+\
     """import numpy as np\n"""+\
     """array=np.array("""+str_array+""")\n"""+\
+    """labels="""+str(labels)+"""\n"""+\
     """print('array dimensions:%s'%str(array.shape))\n"""+\
     """n=array.shape[0]\n"""+\
     """p=sum([list_plot(\n"""+\
-    """    array[i],color=hue(i/n),plotjoined=True,\n"""+\
-    """    marker='o',markersize=2,legend_label=i+1)\n"""+\
+    """    array[i],plotjoined=True,\n"""+\
+    """    color=hue(i/n),marker='o',markersize=2,\n"""+\
+    """    legend_label=labels[i])\n"""+\
     """       for i in range(n)])\n"""+\
     """p.show(figsize=("""+str(
         (np.round(kw*width,2),np.round(kh*height,2)))+\
