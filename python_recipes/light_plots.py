327a2de3ce35d4dfaab71e8c16b152942bda7b9f
Olga Belitskaya <safuolga@gmail.com>
2020-12-05 22:29:55 +0300

    update python_recipes/light_plots.py

--- python_recipes/light_plots.py
+++ python_recipes/light_plots.py
@@ -1,9 +1,19 @@
-import numpy as np,pylab as pl
+import numpy as np,pandas as pd,pylab as pl
 from IPython.core.display import display
+from sklearn.preprocessing import \
+MinMaxScaler as mms
 from lightning import Lightning
-lgn=Lightning(ipython=True,local=True)
 
-def light_line(file_path,file_name,lw,cmap,w,h):
+def lightning_connect(connect):
+    host='https://public.lightning-viz.org'
+    if connect:
+        lgn=Lightning(ipython=True,host=host)
+    else:
+        lgn=Lightning(ipython=True,local=True)
+    return lgn
+
+def light_line(lgn,file_path,file_name,
+               lw=1,cmap='hsv',w=680,h=300):
     series=np.loadtxt(
         file_path+file_name,delimiter=',',skiprows=1)
     if series.shape[0]>series.shape[1]:
@@ -13,4 +23,18 @@ def light_line(file_path,file_name,lw,cmap,w,h):
     c=np.array(255*c,dtype='int32')
     viz=lgn.line(series,color=c,thickness=lw,
                  width=w,height=h)
+    display(viz)
+    
+def light_scatter(lgn,file_path,file_name,
+                  x,y,value,size,label,cmap,
+                  w=680,h=300,sep='\t'):
+    data=pd.read_csv(file_path+file_name,sep=sep)
+    size_array=data[size].values.reshape(-1,1)
+    scaler=mms(feature_range=(3,15)).fit(size_array)
+    size_array=scaler.transform(size_array)
+    viz=lgn.scatter(data[x].values,data[y].values,
+                    values=data[value].values,
+                    labels=data[label].values,
+                    size=size_array.reshape(-1),
+                    colormap=cmap,width=w,height=h)
     display(viz)
