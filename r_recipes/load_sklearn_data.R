15901433fa54f4884090af28ab7c3cd5117350ae
Olga Belitskaya <safuolga@gmail.com>
2020-12-07 22:44:09 +0300

    update r_recipes/load_sklearn_data.R

--- r_recipes/load_sklearn_data.R
+++ r_recipes/load_sklearn_data.R
@@ -4,18 +4,18 @@ X1<-boston$data; y1<-boston$target
 dfboston<-data.frame(boston$data)
 colnames(dfboston)<-boston$feature_names
 dfboston['MEDV']<-boston$target
+ip<-function(x) {as.integer(x)}
+fig<-pl$figure(figsize=c(10,5))
+ax<-fig$add_subplot('111')
+sn$histplot(dfboston['MEDV'],stat='density',ax=ax,
+            kde='True',bins=ip(30),
+            edgecolor='#ff3696',alpha=.1)
+pd$plotting$table(ax,head(dfboston,3),loc='top')
+pl$tight_layout()
+pl$savefig('rpy_p1.png'); im<-load.image('rpy_p1.png')
+options(repr.plot.width=10,repr.plot.height=5)
+par(mar=c(0,0,0,0)); plot(im,axes=FALSE)
 print('Boston Data =>>>')
 print(paste0(c('dim: features -',list(dim(X1)),
                ', target -',list(dim(y1))),
              collapse=' '))
-
-digits<-slds$load_digits()
-X2<-digits$data; y2<-digits$target
-print('Digit Data =>>>')
-print(paste0(c('dim: features -',list(dim(X2)),
-               ', target -',list(dim(y2))),
-             collapse=' '))
-par(mar=c(0,0,0,0)); n<-sample(dim(X2)[1],1)
-im<-array_reshape(X2[n,]/max(X2),c(8,8))
-options(repr.plot.width=3,repr.plot.height=3)
-plot(as.raster(im))
